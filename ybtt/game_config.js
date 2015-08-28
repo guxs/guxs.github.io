0118f00544481810141c3e0ed77634f795f07707
guxs <871576226@qq.com>
2015-08-28 15:55:05 +0800

    ybtt

---
+++ ybtt/game_config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by coman on 15/7/15.
+ */
+var ver = "v10"
+var game_version = "html5/" + ver;
+var game_preload_list = [
+    "launcher/egret_loader.js",
+    "launcher/game-min.js?v=" + ver];
