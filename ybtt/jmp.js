ecf44ecd2a69afe0fcadc208fc7b155aefa03997
guxs <871576226@qq.com>
2015-09-02 19:27:07 +0800

    ybtt

---
+++ ybtt/jmp.js
@@ -0,0 +1,7 @@
+﻿function QueryString(item){
+var sValue=location.search.match(new RegExp("[\?\&]"+item+"=([^\&]*)(\&?)","i"))
+return sValue?sValue[1]:sValue
+};
+if(QueryString("platInfo")==null || QueryString("platInfo")==undefined || QueryString("platInfo")==""){
+	window.location.href="?platInfo=open_258_10319"; 
+}
